dce8754d1db38117ed89fb7cd0479cc10d36f1cb
isaacsantana016 <45479320+isaacsantana016@users.noreply.github.com>
2019-04-09 00:40:43 -0400

    requerido

---
+++ Pryecto-Fi/Pryecto-Fi/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Pryecto_Fi
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Pryecto-Fi/Pryecto-Fi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pryecto_Fi.MvcApplication" Language="C#" %>
